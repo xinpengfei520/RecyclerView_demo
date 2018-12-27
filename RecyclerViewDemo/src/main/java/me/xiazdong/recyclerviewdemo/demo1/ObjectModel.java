cb5111591c8b47612e56642cfb846f067a1c8337
xinpengfei <542270559@qq.com>
2018-12-27 15:23:19 +0800

    delete unuseless code.

--- RecyclerViewDemo/src/main/java/me/xiazdong/recyclerviewdemo/demo1/ObjectModel.java
+++
@@ -1,6 +0,0 @@
-package me.xiazdong.recyclerviewdemo.demo1;
-
-public class ObjectModel {
-    public String title;
-    public int number;
-}
--- RecyclerViewDemo/src/main/java/me/xiazdong/recyclerviewdemo/demo4/Image.java
+++
@@ -1,9 +0,0 @@
-package me.xiazdong.recyclerviewdemo.demo4;
-
-/**
- * Created by xiazdong on 16/10/6.
- */
-public class Image {
-    String name;
-    int id;
-}
